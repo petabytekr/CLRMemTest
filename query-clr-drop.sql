609c512eb769e47611563abd80f64600d425ad71
Jooyoung-Lee <952877+petabytekr@users.noreply.github.com>
2024-05-01 12:11:07 +0900

    Create query-clr-drop.sql

---
+++ query-clr-drop.sql
@@ -0,0 +1,14 @@
+-- 함수 삭제
+DROP FUNCTION IF EXISTS [dbo].TestEmpty;
+DROP FUNCTION IF EXISTS [dbo].TestCallGC;
+DROP FUNCTION IF EXISTS [dbo].TestGetTotalMemory;
+DROP FUNCTION IF EXISTS [dbo].TestLoopConnection;
+DROP FUNCTION IF EXISTS [dbo].TestLoopConnection_CommandNoUsing;
+DROP FUNCTION IF EXISTS [dbo].TestLoopConnection_CommandMultiNoUsing;
+DROP FUNCTION IF EXISTS [dbo].TestLoopConnection_CommandUsing;
+DROP FUNCTION IF EXISTS [dbo].TestLoopConnection_CommandMultiUsing;
+DROP FUNCTION IF EXISTS [dbo].TestEmptyAlloc;
+
+
+-- 어셈블리 삭제 (필요한 경우)
+DROP ASSEMBLY IF EXISTS [CLRMemTest];
