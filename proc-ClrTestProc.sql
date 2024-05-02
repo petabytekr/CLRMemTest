f075c32133b06cae0f76b408dd7c423ed092b61c
Jooyoung-Lee <952877+petabytekr@users.noreply.github.com>
2024-05-02 18:16:31 +0900

    Update proc-ClrTestProc.sql

--- proc-ClrTestProc.sql
+++ proc-ClrTestProc.sql
@@ -14,23 +14,25 @@ BEGIN
 
     SET @StartTime = SYSDATETIME();
 
+    INSERT INTO dbo.log_appdomains SELECT getdate() as checktime, 'START' as logmsg, * FROM sys.dm_clr_appdomains;
+
     INSERT INTO dbo.ProcLog(LogMsg) VALUES ('Starting, Loop=' + CAST(@LoopCount as nvarchar(10)));
 
     INSERT dbo.log_memory_clerks SELECT GETDATE() as checktime, *, 'START' as logmsg FROM sys.dm_os_memory_clerks
-    WHERE type IN ('MEMORYCLERK_SQLBUFFERPOOL','MEMORYCLERK_SQLCLR');
+    -- WHERE type IN ('MEMORYCLERK_SQLBUFFERPOOL','MEMORYCLERK_SQLCLR');
 
     INSERT INTO log_perf_counters SELECT getdate() as checktime, 'START' as logmsg, counter_name, instance_name, mb = cntr_value/1024.0
     FROM sys.dm_os_performance_counters 
-    WHERE (counter_name = N'Cursor memory usage' and instance_name <> N'_Total')
-    OR (instance_name = N'' AND counter_name IN 
-        (N'Connection Memory (KB)', N'Granted Workspace Memory (KB)', 
-            N'Lock Memory (KB)', N'Optimizer Memory (KB)', N'Stolen Server Memory (KB)', 
-            N'Log Pool Memory (KB)', N'Free Memory (KB)')
-    ) ORDER BY mb DESC; 
+    -- WHERE (counter_name = N'Cursor memory usage' and instance_name <> N'_Total')
+    -- OR (instance_name = N'' AND counter_name IN 
+    --     (N'Connection Memory (KB)', N'Granted Workspace Memory (KB)', 
+    --         N'Lock Memory (KB)', N'Optimizer Memory (KB)', N'Stolen Server Memory (KB)', 
+    --         N'Log Pool Memory (KB)', N'Free Memory (KB)')
+    -- ) ORDER BY mb DESC; 
 
     BEGIN TRY
         -- Test 함수 호출
-        SELECT dbo.TestLoopConnection_CommandNoUsing(@LoopCount);
+        SELECT dbo.TestLoopConnection_CommandUsing(@LoopCount);
 
     END TRY
     BEGIN CATCH
@@ -44,17 +46,19 @@ BEGIN
 
     INSERT INTO dbo.ProcLog(LogMsg) VALUES ('End, Loop=' + CAST(@LoopCount as nvarchar(10)) + ', Elapsed=' + CAST(@ElapsedMilliseconds AS nvarchar(10)) + ' ms');
 
-    INSERT dbo.log_memory_clerks SELECT GETDATE() as checktime, 'END' as logmsg, * FROM sys.dm_os_memory_clerks
-    WHERE type IN ('MEMORYCLERK_SQLBUFFERPOOL','MEMORYCLERK_SQLCLR');
+    INSERT INTO dbo.log_appdomains SELECT getdate() as checktime, 'START' as logmsg, * FROM sys.dm_clr_appdomains;
+
+    INSERT dbo.log_memory_clerks SELECT GETDATE() as checktime, *, 'END' as logmsg FROM sys.dm_os_memory_clerks
+    -- WHERE type IN ('MEMORYCLERK_SQLBUFFERPOOL','MEMORYCLERK_SQLCLR');
 
     INSERT INTO log_perf_counters SELECT getdate() as checktime, 'END' as logmsg, counter_name, instance_name, mb = cntr_value/1024.0
     FROM sys.dm_os_performance_counters 
-    WHERE (counter_name = N'Cursor memory usage' and instance_name <> N'_Total')
-    OR (instance_name = N'' AND counter_name IN 
-        (N'Connection Memory (KB)', N'Granted Workspace Memory (KB)', 
-            N'Lock Memory (KB)', N'Optimizer Memory (KB)', N'Stolen Server Memory (KB)', 
-            N'Log Pool Memory (KB)', N'Free Memory (KB)')
-    ) ORDER BY mb DESC; 
+    -- WHERE (counter_name = N'Cursor memory usage' and instance_name <> N'_Total')
+    -- OR (instance_name = N'' AND counter_name IN 
+    --     (N'Connection Memory (KB)', N'Granted Workspace Memory (KB)', 
+    --         N'Lock Memory (KB)', N'Optimizer Memory (KB)', N'Stolen Server Memory (KB)', 
+    --         N'Log Pool Memory (KB)', N'Free Memory (KB)')
+    -- ) ORDER BY mb DESC; 
 
 END;
 GO
